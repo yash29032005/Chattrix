c927da27fc67fcc08c0a809d7fec13ba5ee34b36
Yash panchal <yashpanchal2905@gmail.com>
2025-08-02 12:13:43 +0530

    added green dot

--- client/src/components/sidebar/usercard.js
+++ client/src/components/sidebar/usercard.js
@@ -2,29 +2,49 @@ import { usePage } from "../../context/pagecontext";
 
 function Usercard({ otheruser }) {
   const { setShowmessagesection, setSelecteduser } = usePage();
+
   return (
     <>
       <div
-        style={{ height: "15%", background: "var(--tertiary-color)" }}
-        className="rounded-4 d-flex align-items-center mb-4 p-3 "
+        style={{
+          height: "15%",
+          background: "var(--tertiary-color)",
+          position: "relative",
+        }}
+        className="rounded-4 d-flex align-items-center mb-4 p-3"
         onClick={() => {
           setShowmessagesection(true);
           setSelecteduser(otheruser);
         }}
       >
-        <img
-          style={{
-            height: "50px",
-            width: "50px",
-            background: "var(--primary-color)",
-          }}
-          className="rounded-circle"
-          src={`https://robohash.org/${otheruser.username}`}
-          alt="profilepic"
-        />
+        <div style={{ position: "relative" }}>
+          <img
+            style={{
+              height: "50px",
+              width: "50px",
+              background: "var(--primary-color)",
+            }}
+            className="rounded-circle"
+            src={`https://robohash.org/${otheruser.username}`}
+            alt="profilepic"
+          />
+          <span
+            style={{
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              height: "12px",
+              width: "12px",
+              backgroundColor: "#28a745",
+              border: "2px solid white",
+              borderRadius: "50%",
+            }}
+          />
+        </div>
         <h5 className="ms-4 mt-2">{otheruser.fullname}</h5>
       </div>
     </>
   );
 }
+
 export default Usercard;
